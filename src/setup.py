454566ffd4b6bdcf4a0dc43548ca8c05f4931e66
Your Name <you@example.com>
2026-04-01 03:52:32 +0000

    Adapt repository layout and template files

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import setup_translate
+
+pkg = 'SystemPlugins.XtrendRemote'
+setup(name='enigma2-plugin-systemplugins-remotecontrolcode',
+       version='3.0',
+       description='Change Remote Control Code, Switch ET5000 ET9000 DMM remote',
+       package_dir={pkg: 'XtrendRemote'},
+       packages=[pkg],
+       package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo']},
+       cmdclass=setup_translate.cmdclass,  # for translation
+      )
